b336738318a69cc033442271b0b238dcbad77c9b
Sidharth <143301582+Sid-CodeX@users.noreply.github.com>
2025-05-22 22:16:01 +0530

    Update classroom.js

--- backend/routes/classroom.js
+++ backend/routes/classroom.js
@@ -19,7 +19,7 @@ const authenticate = (req, res, next) => {
   }
 };
 
-//  POST /create-classroom
+// POST /create-classroom
 router.post("/create-classroom", authenticate, async (req, res) => {
   try {
     const { name, description } = req.body;
