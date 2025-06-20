707f0c0b25a21103cb5d14e3555411805b5207d1
Sidharth P <sidhusaketham@gmail.com>
2025-06-20 18:50:08 +0530

    feat: Added 'submissionStatus' and 'submissionDate' to the Assignment model

--- backend/models/Assignment.js
+++ backend/models/Assignment.js
@@ -1,47 +1,48 @@
 const mongoose = require("mongoose");
 
 const submissionSchema = new mongoose.Schema({
-  studentId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  name: String,
-  email: String,
-  submitted: { type: Boolean, default: false },
-  submittedAt: { type: Date },
-  fileName: String,
-  fileSize: { type: Number },
-  extractedText: String,
-  plagiarismPercent: Number,
-  wordCount: Number,
-  teacherRemark: { type: String, default: "No remarks" },
-  minHashSignature: { type: [Number], default: [] },
-  late: { type: Boolean, default: false },
+    studentId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    name: String,
+    email: String,
+    submitted: { type: Boolean, default: false },
+    submittedAt: { type: Date },
+    fileName: String,
+    fileSize: { type: Number },
+    extractedText: String,
+    plagiarismPercent: { type: Number }, 
+    wordCount: Number,
+    teacherRemark: { type: String, default: "No remarks" }, 
+    minHashSignature: { type: [Number], default: [] },
+    late: { type: Boolean, default: false }, 
 
-  topMatches: [{
-    matchedStudentId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    matchedText: String,
-    plagiarismPercent: Number
-  }],
+    topMatches: [{
+        matchedStudentId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        matchedText: String,
+        plagiarismPercent: Number
+    }],
 
-  allMatches: [{
-    matchedStudentId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    plagiarismPercent: Number
-  }]
+    allMatches: [{
+        matchedStudentId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        plagiarismPercent: Number
+    }],
 });
 
 const assignmentSchema = new mongoose.Schema({
-  classroomId: { type: mongoose.Schema.Types.ObjectId, ref: "Classroom", required: true },
-  type: { type: String, enum: ["Assignment", "Exam"], required: true },
-  title: { type: String, required: true },
-  description: String,
-  deadline: { type: Date, required: true },
-  questionFile: {
-    data: Buffer,
-    contentType: String,
-    originalName: String
-  },
-  
-  submissions: [submissionSchema],
-  createdAt: { type: Date, default: Date.now }
+    classroomId: { type: mongoose.Schema.Types.ObjectId, ref: "Classroom", required: true },
+    type: { type: String, enum: ["Assignment", "Exam"], required: true },
+    title: { type: String, required: true },
+    description: String,
+    deadline: { type: Date, required: true },
+    questionFile: {
+        data: Buffer,
+        contentType: String,
+        originalName: String
+    },
+    canSubmitLate: { type: Boolean, default: true }, 
+    submissionGuidelines: { type: [String], default: [] }, 
+
+    submissions: [submissionSchema],
+    createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.models.Assignment || mongoose.model('Assignment', assignmentSchema);
-
